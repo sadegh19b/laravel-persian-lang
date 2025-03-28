fdc51b586bfc177a88eef37bb67f3084bab4c790
Sadegh Barzegar <sadegh19b@gmail.com>
2025-03-28 12:43:46 +0330

    feat: upgrade to laravel 6

--- src/PersianLangServiceProvider.php
+++ src/PersianLangServiceProvider.php
@@ -23,17 +23,8 @@ class PersianLangServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (method_exists($this, 'publishes')) {
-            // For Laravel 5.3+
-            $this->publishes([
-                __DIR__.'/../lang/fa' => base_path('resources/lang/fa'),
-            ], 'persian-lang');
-        }else {
-            // For Laravel 5.0-5.2
-            $this->app['files']->copyDirectory(
-                __DIR__.'/../lang/fa',
-                base_path('resources/lang/fa')
-            );
-        }
+        $this->publishes([
+            __DIR__.'/../lang/fa' => resource_path('lang/fa'),
+        ], 'persian-lang');
     }
 }
